252d6d543913d8579f2d96fd387eef4733690634
Piitaaraq <peter.lovstrom@gmail.com>
2025-02-12 14:27:42 +0100

    Celery worker naming fix

--- dgp_bus_project/celery.py
+++ dgp_bus_project/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dgp_bus_project.settings')
 
-app = Celery('dgp_bus')
+app = Celery('dgp_bus_project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
