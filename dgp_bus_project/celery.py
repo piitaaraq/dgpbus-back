53387ea4814f2cdbeb664a70a49ed7f241a2faea
Piitaaraq <peter.lovstrom@gmail.com>
2025-04-14 20:19:50 +0200

    Adding celery

--- dgp_bus_project/celery.py
+++ dgp_bus_project/celery.py
@@ -6,5 +6,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dgp_bus_project.settings')
 
 app = Celery('dgp_bus_project')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.beat_scheduler = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 app.autodiscover_tasks()
